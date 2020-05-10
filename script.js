ab69c30770bf83423a4520dd4e3be27f54d22761
wazhma shaiwayana <wazhma.shaiwayana@gmail.com>
2020-05-09 22:51:03 -0700

    changes

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
             url: "http://api.openweathermap.org/data/2.5/weather?q=" + city.val() + "&appid=" + "daa70b31f7c16b78d4c41d748cc4a1cb" + "&units=imperial",
             dataType: "json",
             success: function(response) {
-              console.log("response", (response));
+             // console.log("response", (response));
 
                  $(".show").empty();
          
@@ -42,29 +42,53 @@ $(document).ready(function(){
 
         
         $.ajax({
-        type: "GET",
-        url: "https://www.api.openweathermap.org/data/2.5/forecast?q=san%20diego&appid=daa70b31f7c16b78d4c41d748cc4a1cb",
-         dataType: "json",
-         success: function(response) {
-
-            console.log("daily response", response);
+            type: "GET",
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city.val() + "&appid=" + "daa70b31f7c16b78d4c41d748cc4a1cb",
+            dataType: "json",
+            success: function(response) {
+            console.log("forecast", response);
                 
-                $(".show").empty();
 
-                var humidityDaily = $("<p>").text("humidity:  " + response.daily.humidity);
-                $(".forcast").append(humidityDaily);
+                 var humidityDaily = $("<p>").text("humidity:  " + response.list[5].main.humidity);
+                $(".forecast").append(humidityDaily);
 
-                var UVINDEX= $("<p>").text("UV index:  " + response.daily.uvi);
-                $(".show").append(UVINDEX);
+                var tempDaily = $("<p>").text("temp:  " + response.list[3].main.temp);
+                $(".forecast").append(tempDaily);
 
-                var tempDaily = $("<p>").text("temp:  " + response.daily.temp);
-                $(".show").append(tempDaily);
+                var day1 = $("<p>").text(response.list[0].dt_txt);
+                 $(".day1").append(day1);
 
+                var day2 = $("<p>").text( response.list[7].dt_txt);
+                $(".day2").append(day2);
+
+                var day3 = $("<p>").text(response.list[15].dt_txt);
+                $(".day3").append(day3);
+
+                 var day4 = $("<p>").text(response.list[23].dt_txt);
+                $(".day4").append(day4);
+
+                var day5= $("<p>").text(response.list[31].dt_txt);
+                $(".day5").append(day5);
+
+
+                // i tried to add this like the one above to clear out the previous data but it made the code not work completely for this part. 
+                //$(".daily").empty();
+                
+                
+                //tried to add the icons here but it wouldnt work
+                // var icons=$("<img>").html(response.list[0].weather[0].icon);
+                // $(".forecast").append(icons);
             }
+
+            // this part was for the uv index but this part didnt work either. 
+            // var UVINDEX= $("<p>").text("UV index:  " + response.daily.uvi);
+            // $(".show").append(UVINDEX);
 
                 
         });
 
     });
 })
+
+
 
